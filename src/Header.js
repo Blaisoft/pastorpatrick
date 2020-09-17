a1cf0f536e5ff3a94697c32f815c26fb3e06124b
EB-COMPANY <Munezero>
2020-09-16 23:47:26 -0700

    blaisoft

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
             <div>
    <nav className="navbar navbar-default navbar-static-top m-b-0">
     <div className="navbar-header"> <a className="navbar-toggle hidden-sm hidden-md hidden-lg " href="javascript:void(0)" data-toggle="collapse" data-target=".navbar-collapse"><i className="ti-menu" /></a>
-    <div className="top-left-part"><a className="logo" href="/"><b><img src="../plugins/images/users/1.jpg" alt="logo" /></b><span className="hidden-xs"><strong>Pastor</strong>Patrick</span></a></div>
+    <div className="top-left-part"><a className="logo" href="/"><b><img src="" alt="logo" /></b><span className="hidden-xs"><strong>Pastor</strong>Patrick</span></a></div>
     <ul className="nav navbar-top-links navbar-left hidden-xs">
       <li><a href="javascript:void(0)" className="open-close hidden-xs waves-effect waves-light"><i className="icon-arrow-left-circle ti-menu" /></a></li>
       <li>
